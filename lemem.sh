a45e68269236435f6012d348db5c012df1c4705a
mrbee nopro <127410361+mrbeeenopro@users.noreply.github.com>
2025-06-09 23:12:06 +0700

    Update lemem.sh

--- lemem.sh
+++ lemem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 [ -z "$HOME" ] && [ -d "/home/container" ] && export HOME="/home/container"
-export HOSTNAME="lemem-aphine"
+export HOSTNAME="$(cat /proc/sys/kernel/hostname)"
 
 # Copyright (c) by H3L2F. All rights reserved.
 # https://h3l2f.site
